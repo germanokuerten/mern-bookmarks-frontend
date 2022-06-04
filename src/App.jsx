18b077442e9f8ab9a5b5292bf7603b42aea697a8
Alejandro Rojas <kmachappy@pm.me>
2022-06-04 12:05:05 -0700

    added getBookmarks function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import Body from './components/body';
 import Form from './components/form';
 import Header from './components/header';
 
 function App() {
+  const[bookmarks, setBookmarks] = useState(null)
+
+  // url
+  const url = 'https://buenas-noches-backend.herokuapp.com/'
+
+  //
+  
+  // const getBookmarks = async () =>{
+  //   const data = await fetch(url+'bookmark').then(res => res.json())
+  //     console.log(data)
+  // }
+
+  async function getBookmarks(){
+    //fetch database 
+    // to get our bookmarks
+    try {
+      const data = await fetch(url+'bookmark').then(res => res.json())
+      console.log(data)
+      setBookmarks(data)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{getBookmarks()},[])
+
   return (
     <div className="App">
       <Header/>
